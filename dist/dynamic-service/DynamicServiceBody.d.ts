98e76c1e1acdc7be2ca60ee3bc4a5cdfe9fb90ac
alecejones-noodoe <alecejones@noodoe.com>
2022-03-01 17:14:53 +0800

    First commit.

---
+++ dist/dynamic-service/DynamicServiceBody.d.ts
@@ -0,0 +1,28 @@
+export interface DynamicServiceBody {
+    plugin: {
+        name: string;
+        action: string;
+        script: string;
+    };
+    pipeline: {
+        pipelineId: string;
+        pipelineJobId: string;
+        pipelineNodeId: string;
+        pipelineNode: Record<string, any>;
+    };
+    site: {
+        site?: Record<string, any>;
+        category?: Record<string, any>;
+    };
+    input: {
+        itemsFromParent?: Record<string, any>[];
+        filesFromParent?: Record<string, any>[];
+        tokensFromParent?: Record<string, any>[];
+        repositoryItems?: Record<string, any>[];
+        repositoryItemIds?: string[];
+        repositoryItemCount?: number;
+    };
+    parameters: {
+        conditionFailed: string[];
+    };
+}
