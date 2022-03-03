6e06c36e1fb28da54ff60a9e69668cef0de7ea41
alecejones-noodoe <alecejones@noodoe.com>
2022-03-03 23:14:51 +0800

    Refactor return values of module factory

--- dist/plugin-module/PluginModuleFactory.d.ts
+++ dist/plugin-module/PluginModuleFactory.d.ts
@@ -1,17 +1,20 @@
 import { DynamicServiceBody, DynamicServiceResponseOutput, DynamicServiceResponseAuthCallback, DynamicServiceResponseListener } from "..";
+export declare type ActionReturnValue = Promise<DynamicServiceResponseOutput>;
 export declare type PluginModuleActionFactory = () => {
     [n: string]: {
-        main: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseOutput>;
+        main: (body: DynamicServiceBody, lib: any) => ActionReturnValue;
     };
 };
+export declare type CredentialReturnValue = Promise<DynamicServiceResponseAuthCallback>;
 export declare type PluginModuleCredentialFactory = () => {
     [n: string]: {
-        authCallback: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseAuthCallback>;
-        test: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseAuthCallback>;
+        authCallback: (body: DynamicServiceBody, lib: any) => CredentialReturnValue;
+        test: (body: DynamicServiceBody, lib: any) => CredentialReturnValue;
     };
 };
+export declare type ListenerReturnValue = Promise<DynamicServiceResponseListener>;
 export declare type PluginModuleListenerFactory = () => {
     [n: string]: {
-        main: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseListener>;
+        main: (body: DynamicServiceBody, lib: any) => ListenerReturnValue;
     };
 };
--- src/plugin-module/PluginModuleFactory.ts
+++ src/plugin-module/PluginModuleFactory.ts
@@ -1,20 +1,26 @@
 import { DynamicServiceBody, DynamicServiceResponseOutput, DynamicServiceResponseAuthCallback, DynamicServiceResponseListener } from ".."
 
+export type ActionReturnValue = Promise<DynamicServiceResponseOutput>
+
 export type PluginModuleActionFactory = () => {
     [n: string]: {
-        main: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseOutput>
+        main: (body: DynamicServiceBody, lib: any) => ActionReturnValue
     }
 }
+
+export type CredentialReturnValue = Promise<DynamicServiceResponseAuthCallback>
 
 export type PluginModuleCredentialFactory = () => {
     [n: string]: {
-        authCallback: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseAuthCallback>
-        test: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseAuthCallback>
+        authCallback: (body: DynamicServiceBody, lib: any) => CredentialReturnValue
+        test: (body: DynamicServiceBody, lib: any) => CredentialReturnValue
     }
 }
 
+export type ListenerReturnValue = Promise<DynamicServiceResponseListener>
+
 export type PluginModuleListenerFactory = () => {
     [n: string]: {
-        main: (body: DynamicServiceBody, lib: any) => Promise<DynamicServiceResponseListener>
+        main: (body: DynamicServiceBody, lib: any) => ListenerReturnValue
     }
 }
