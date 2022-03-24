c7b6a810ae223ae56cccfda892eed8cfff4ea89e
alecejones-noodoe <alecejones@noodoe.com>
2022-03-24 18:37:51 +0800

    Improved dyanmic service body parameters

--- dist/dynamic-service/DynamicServiceBodyParameters.d.ts
+++ dist/dynamic-service/DynamicServiceBodyParameters.d.ts
@@ -1,3 +1,16 @@
+import { PipelineMessage } from "..";
+import { FileMetadata } from "..";
+import { RepositoryItem } from "..";
+import { RepositoryFile } from "..";
+import { PipelineToken } from "..";
 export interface DynamicServiceBodyParameters {
+    status: string;
+    recordReports: boolean;
+    messagesFromParent: PipelineMessage[];
+    fileMetadataFromParent: FileMetadata[];
+    filesFromParent: RepositoryFile[];
+    itemsFromParent: RepositoryItem[];
+    tokensFromParent: PipelineToken;
+    conditionFailedFromParent: string[];
     conditionFailed: string[];
 }
