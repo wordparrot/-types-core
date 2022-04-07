0b383feb1ddefc32f93cbf0f093c7161e79cd77d
alecejones-noodoe <alecejones@noodoe.com>
2022-04-07 20:57:28 +0800

    modify plugin module factory

--- src/plugin-module/PluginModuleFactory.ts
+++ src/plugin-module/PluginModuleFactory.ts
@@ -1,42 +1,52 @@
-import { 
-    DynamicServiceBody, 
-    DynamicServiceResponseOutput, 
-    DynamicServiceResponseOutputAuthCallback, 
-    DynamicServiceResponseOutputListener,
-    DynamicServiceResponseOutputWebhook,
-    DynamicServiceBodyWebhook,
+import {
+  DynamicServiceBody,
+  DynamicServiceResponseOutput,
+  DynamicServiceResponseOutputAuthCallback,
+  DynamicServiceResponseOutputListener,
+  DynamicServiceResponseOutputWebhook,
+  DynamicServiceBodyWebhook,
 } from "..";
-import { SandboxLib } from ".."
+import { SandboxLib } from "..";
 
-export type ActionReturnValue = Promise<DynamicServiceResponseOutput>
+export type ActionReturnValue = Promise<DynamicServiceResponseOutput>;
 
 export type PluginModuleActionFactory = () => {
-    [n: string]: {
-        main: (body?: DynamicServiceBody, lib?: SandboxLib) => ActionReturnValue | ListenerReturnValue
-    }
-}
+  [n: string]: {
+    main: (
+      body?: DynamicServiceBody,
+      lib?: SandboxLib
+    ) => ActionReturnValue | ListenerReturnValue;
+  };
+};
 
-export type CredentialReturnValue = Promise<DynamicServiceResponseOutputAuthCallback>
+export type CredentialReturnValue =
+  Promise<DynamicServiceResponseOutputAuthCallback>;
 
 export type PluginModuleCredentialFactory = () => {
-    [n: string]: {
-        authCallback: (body?: DynamicServiceBody, lib?: SandboxLib) => CredentialReturnValue
-        test: (body?: DynamicServiceBody, lib?: SandboxLib) => CredentialReturnValue
-    }
-}
+  [n: string]: {
+    authCallback: (
+      body?: DynamicServiceBody,
+      lib?: SandboxLib
+    ) => CredentialReturnValue;
+    test: (
+      body?: DynamicServiceBody,
+      lib?: SandboxLib
+    ) => CredentialReturnValue;
+  };
+};
 
-export type ListenerReturnValue = Promise<DynamicServiceResponseOutputListener>
+export type ListenerReturnValue = Promise<DynamicServiceResponseOutputListener>;
 
 export type PluginModuleListenerFactory = () => {
-    [n: string]: {
-        main: (body?: DynamicServiceBody, lib?: SandboxLib) => ListenerReturnValue
-    }
-}
+  [n: string]: {
+    main: (body?: DynamicServiceBody, lib?: SandboxLib) => ListenerReturnValue;
+  };
+};
 
-export type WebhookReturnValue = Promise<DynamicServiceResponseOutputWebhook>
+export type WebhookReturnValue = Promise<DynamicServiceResponseOutputWebhook>;
 
 export type PluginModuleWebhookFactory = () => {
-    [n: string]: {
-        main: (body?: DynamicServiceBodyWebhook, lib?: SandboxLib) => WebhookReturnValue
-    }
-}
+  [n: string]: {
+    main: (body?: DynamicServiceBody, lib?: SandboxLib) => WebhookReturnValue;
+  };
+};
