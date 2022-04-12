1458397073c8b2f3922b35fa8c1cdecd0d7bd3b9
alecejones-noodoe <alecejones@noodoe.com>
2022-04-13 00:48:17 +0800

    add property_unique csv report cell value

--- dist/csv-report/CsvReportCell.d.ts
+++ dist/csv-report/CsvReportCell.d.ts
@@ -1,14 +1,21 @@
 import { CsvReportCellType } from ".";
 export declare type CsvReportCellData = Record<string, CsvReportCell>;
+export interface CsvReportCellCondition {
+    p: string;
+    v: string;
+    c: string;
+    r: string;
+}
 export interface CsvReportCell {
     y: number;
     x: number;
-    v: any;
-    t: CsvReportCellType;
+    t: CsvReportCellType | null;
+    v?: any;
     d?: string | number;
     p?: string;
     a?: string;
     bk?: boolean;
     bp?: string;
     s?: string;
+    c?: Record<string, CsvReportCellCondition>[];
 }
--- src/csv-report/CsvReportCell.ts
+++ src/csv-report/CsvReportCell.ts
@@ -2,15 +2,23 @@ import { CsvReportCellType } from ".";
 
 export type CsvReportCellData = Record<string, CsvReportCell>;
 
+export interface CsvReportCellCondition {
+  p: string; // relevant property
+  v: string; // conditional value
+  c: string; // condition (greater than, less than, equals, before, after)
+  r: string; // relation (AND, OR)
+}
+
 export interface CsvReportCell {
   y: number; // y-index
   x: number; //  x-index
-  v: any; // value
-  t: CsvReportCellType; // type
+  t: CsvReportCellType | null; // type
+  v?: any; // value
   d?: string | number; // ISO or timestamp representing a date. Also accepts special value strings like 'today' or 'one week ago'.
   p?: string; // property
   a?: string; // property alias
   bk?: boolean; // by key? - print the rowKey of a matching row, instead of the value itself.
-  bp?: string; // by property? - print the value of a matching row's property, instead of the value itself. For example, 'title' for a movie that has max viewers among a set.
+  bp?: string; // by properties? - print the interpolated value of a property, such as `{total} - {name}`, instead of the value itself. For example, 'title' for a movie that has max viewers among a set.
   s?: string; // separator, such as a hyphen, comma, semicolon, or slash
+  c?: Record<string, CsvReportCellCondition>[];
 }
