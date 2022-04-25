6cbe700743be4ff53bbdcb27de49b4c216c15182
alecejones-noodoe <alecejones@noodoe.com>
2022-04-25 18:51:47 +0800

    make pipeline redis not mandatory

--- src/dynamic-service/DynamicServiceBodyPipeline.ts
+++ src/dynamic-service/DynamicServiceBodyPipeline.ts
@@ -1,10 +1,10 @@
 import { PipelineNode } from "..";
-import { PipelineRedis } from '..'
+import { PipelineRedis } from "..";
 
 export interface DynamicServiceBodyPipeline {
-    pipelineId: string
-    pipelineJobId: string
-    pipelineNodeId: string
-    pipelineNode: PipelineNode
-    pipelineRedis: PipelineRedis
+  pipelineId: string;
+  pipelineJobId?: string;
+  pipelineNodeId: string;
+  pipelineNode: PipelineNode;
+  pipelineRedis?: PipelineRedis;
 }
