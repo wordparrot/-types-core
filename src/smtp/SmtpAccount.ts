f57089ee8fbf7e7519988ad9180e5bbbb90ca92a
alecejones-noodoe <alecejones@noodoe.com>
2022-05-07 14:00:19 +0800

    Add smtp

---
+++ src/smtp/SmtpAccount.ts
@@ -0,0 +1,15 @@
+import { String } from "lodash";
+
+export interface SmtpAccount {
+  id: string;
+  title: string;
+  content: string;
+  active: boolean;
+  smtpProvider: string;
+  port: number;
+  domain: string;
+  apiKey: string;
+  sender: String;
+  createdAt: string;
+  updatedAt: string;
+}
