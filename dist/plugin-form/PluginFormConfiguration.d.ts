5fad66c2b59bbf0443d49d6f2294f5adea6ba500
alecejones-noodoe <alecejones@noodoe.com>
2022-05-15 17:13:20 +0800

    add blueprint to form.json

--- dist/plugin-form/PluginFormConfiguration.d.ts
+++ dist/plugin-form/PluginFormConfiguration.d.ts
@@ -3,9 +3,11 @@ import { CredentialFormConfiguration } from '.';
 import { ListenerFormConfiguration } from '.';
 import { FormConfigurationUsingActionForm } from '.';
 import { WebhookFormConfiguration } from '.';
+import { Blueprint } from '../blueprint';
 export interface PluginFormConfiguration {
     actions: ActionFormConfiguration[];
     credentials?: (CredentialFormConfiguration | FormConfigurationUsingActionForm)[];
     listeners?: (ListenerFormConfiguration | FormConfigurationUsingActionForm)[];
     webhooks?: (WebhookFormConfiguration | FormConfigurationUsingActionForm)[];
+    blueprints?: Blueprint[];
 }
--- src/plugin-form/PluginFormConfiguration.ts
+++ src/plugin-form/PluginFormConfiguration.ts
@@ -1,12 +1,17 @@
-import { ActionFormConfiguration } from '.'
-import { CredentialFormConfiguration } from '.'
-import { ListenerFormConfiguration } from '.'
-import { FormConfigurationUsingActionForm } from '.'
-import { WebhookFormConfiguration } from '.'
+import { ActionFormConfiguration } from ".";
+import { CredentialFormConfiguration } from ".";
+import { ListenerFormConfiguration } from ".";
+import { FormConfigurationUsingActionForm } from ".";
+import { WebhookFormConfiguration } from ".";
+import { Blueprint } from "../blueprint";
 
 export interface PluginFormConfiguration {
-    actions: ActionFormConfiguration[]
-    credentials?: (CredentialFormConfiguration | FormConfigurationUsingActionForm)[]
-    listeners?: (ListenerFormConfiguration | FormConfigurationUsingActionForm)[]
-    webhooks?: (WebhookFormConfiguration | FormConfigurationUsingActionForm)[]
+  actions: ActionFormConfiguration[];
+  credentials?: (
+    | CredentialFormConfiguration
+    | FormConfigurationUsingActionForm
+  )[];
+  listeners?: (ListenerFormConfiguration | FormConfigurationUsingActionForm)[];
+  webhooks?: (WebhookFormConfiguration | FormConfigurationUsingActionForm)[];
+  blueprints?: Blueprint[];
 }
