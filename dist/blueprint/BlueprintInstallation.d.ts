acd3915e64da95ea06dd0e225bb10656d01a25dc
alecejones-noodoe <alecejones@noodoe.com>
2022-06-11 13:53:34 +0800

    Add blueprint form data stuff

--- dist/blueprint/BlueprintInstallation.d.ts
+++ dist/blueprint/BlueprintInstallation.d.ts
@@ -9,3 +9,48 @@ export interface BlueprintInstallation {
     createdAt: string;
     updatedAt: string;
 }
+export interface FormDataInnerValue {
+    index: number;
+    isValid: boolean;
+    values: any;
+}
+export interface FormDataItem {
+    title: string;
+    type: 'pipeline';
+    inner: Record<string, FormDataInnerValue>;
+    allReady: boolean;
+}
+export interface WebhookFormDataItem {
+    title: string;
+    type: 'webhook';
+    values: any;
+    isValid: boolean;
+}
+export interface ListenerFormDataItem {
+    title: string;
+    type: 'listener';
+    values: any;
+    isValid: boolean;
+}
+export interface FormDataItemToChange {
+    title: string;
+    index: number;
+    innerValue: FormDataInnerValue;
+    allReady: boolean;
+}
+export interface WebhookFormDataItemToChange {
+    title: string;
+    index: number;
+    isValid: boolean;
+    values: any;
+}
+export declare type ListenerFormDataItemToChange = WebhookFormDataItemToChange;
+export declare type FormDataMap = Record<string, FormDataItem>;
+export declare type WebhookFormDataMap = Record<string, WebhookFormDataItem>;
+export declare type ListenerFormDataMap = Record<string, ListenerFormDataItem>;
+export interface BlueprintInstallationToSave {
+    blueprintInstallationId: string;
+    pipelines: FormDataMap;
+    webhooks: WebhookFormDataMap;
+    listeners: ListenerFormDataMap;
+}
--- src/blueprint/BlueprintInstallation.ts
+++ src/blueprint/BlueprintInstallation.ts
@@ -15,3 +15,59 @@ export interface BlueprintInstallation {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface FormDataInnerValue {
+  index: number;
+  isValid: boolean;
+  values: any;
+}
+
+export interface FormDataItem {
+  title: string;
+  type: "pipeline";
+  inner: Record<string, FormDataInnerValue>;
+  allReady: boolean;
+}
+
+export interface WebhookFormDataItem {
+  title: string;
+  type: "webhook";
+  values: any;
+  isValid: boolean;
+}
+
+export interface ListenerFormDataItem {
+  title: string;
+  type: "listener";
+  values: any;
+  isValid: boolean;
+}
+
+export interface FormDataItemToChange {
+  title: string;
+  index: number;
+  innerValue: FormDataInnerValue;
+  allReady: boolean;
+}
+
+export interface WebhookFormDataItemToChange {
+  title: string;
+  index: number;
+  isValid: boolean;
+  values: any;
+}
+
+export type ListenerFormDataItemToChange = WebhookFormDataItemToChange;
+
+export type FormDataMap = Record<string, FormDataItem>;
+
+export type WebhookFormDataMap = Record<string, WebhookFormDataItem>;
+
+export type ListenerFormDataMap = Record<string, ListenerFormDataItem>;
+
+export interface BlueprintInstallationToSave {
+  blueprintInstallationId: string;
+  pipelines: FormDataMap;
+  webhooks: WebhookFormDataMap;
+  listeners: ListenerFormDataMap;
+}
