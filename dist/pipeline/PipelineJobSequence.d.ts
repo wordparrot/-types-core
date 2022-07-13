e86909e39f068a3f10561052bd7ba222393fb69f
alecejones-noodoe <alecejones@noodoe.com>
2022-07-13 14:58:40 +0800

    add prompt/listener/webhook to sequence

--- dist/pipeline/PipelineJobSequence.d.ts
+++ dist/pipeline/PipelineJobSequence.d.ts
@@ -1,7 +1,16 @@
 import { PipelineJob } from ".";
+import { Listener } from "../listener";
+import { Prompt } from "../prompt";
+import { Webhook } from "../webhook";
 export interface PipelineJobSequence {
     id: string;
     status: string;
+    prompt: Prompt;
+    promptId: string;
+    listener: Listener;
+    listenerId: string;
+    webhook: Webhook;
+    webhookId: string;
     jobs: PipelineJob[];
     completedAt: string;
     createdAt: string;
