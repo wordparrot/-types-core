69126cbba0412ba03c6e815bc6b8f6f58ad4a7ec
alecejones-noodoe <alecejones@noodoe.com>
2022-08-17 00:24:49 +0800

    fix: change batch process

--- dist/pipeline/PipelineNodeBatchProcess.d.ts
+++ dist/pipeline/PipelineNodeBatchProcess.d.ts
@@ -1,13 +1,15 @@
-import { PipelineNode, PipelineJob } from ".";
+import { PipelineNode, PipelineJob, PipelineNodeReport, PipelineRedis } from ".";
+import { DynamicServiceTrackedValues } from "..";
+import { BatchResults } from "../_classes";
 export interface PipelineNodeBatchProcess {
     id: string;
     status: string;
     batchIndex: number;
     batchSize: number;
-    batchState: any;
-    parentTrackedValues: any;
-    pipelineRedis?: any;
-    diff?: any;
+    batchResults: BatchResults;
+    parentTrackedValues: DynamicServiceTrackedValues;
+    pipelineRedis?: PipelineRedis;
+    diff?: PipelineNodeReport['diff'];
     pipelineJob?: PipelineJob;
     pipelineJobId: string;
     pipelineNode?: PipelineNode;
--- src/pipeline/PipelineNodeBatchProcess.ts
+++ src/pipeline/PipelineNodeBatchProcess.ts
@@ -1,4 +1,11 @@
-import { PipelineNode, PipelineJob } from ".";
+import {
+  PipelineNode,
+  PipelineJob,
+  PipelineNodeReport,
+  PipelineRedis,
+} from ".";
+import { DynamicServiceTrackedValues } from "..";
+import { BatchResults } from "../_classes";
 
 export interface PipelineNodeBatchProcess {
   id: string;
@@ -6,10 +13,10 @@ export interface PipelineNodeBatchProcess {
   batchIndex: number;
   batchSize: number;
 
-  batchState: any;
-  parentTrackedValues: any;
-  pipelineRedis?: any;
-  diff?: any;
+  batchResults: BatchResults;
+  parentTrackedValues: DynamicServiceTrackedValues;
+  pipelineRedis?: PipelineRedis;
+  diff?: PipelineNodeReport["diff"];
 
   pipelineJob?: PipelineJob;
   pipelineJobId: string;
