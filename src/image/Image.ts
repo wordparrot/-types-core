298b1f3c80083cb0e7ce664ca1fb87e3f2571288
alecejones-noodoe <alecejones@noodoe.com>
2023-03-04 17:48:23 -0600

    fix: add image filename

--- src/image/Image.ts
+++ src/image/Image.ts
@@ -1,16 +1,17 @@
-import { Site } from '..'
-import { User } from '..'
-import { Entry } from '..'
-import { Banner } from '..'
-import { FeaturedImage } from '..'
+import { Site } from "..";
+import { User } from "..";
+import { Entry } from "..";
+import { Banner } from "..";
+import { FeaturedImage } from "..";
 
-export type ImageType = 'hosted' | 'external'
+export type ImageType = "hosted" | "external";
 
 export interface Image {
   id: string;
   uniqId: string;
   title: string;
   subtitle: string;
+  filename: string;
   url: string;
   size: number;
   height: number;
@@ -19,11 +20,11 @@ export interface Image {
   status: string;
   type: ImageType;
 
-  site: Site
-  user: User
-  entries: Entry[]
-  banners: Banner[]
-  featuredImages: FeaturedImage[]
+  site: Site;
+  user: User;
+  entries: Entry[];
+  banners: Banner[];
+  featuredImages: FeaturedImage[];
 
   createdAt?: string;
   updatedAt?: string;
