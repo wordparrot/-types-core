387deff483183ae57b54533da22f9c35bbf780dd
alecejones-noodoe <alecejones@noodoe.com>
2023-04-17 18:21:24 -0500

    feat: add mobile project user config

---
+++ dist/project/ProjectUserConfiguration.d.ts
@@ -0,0 +1,11 @@
+import { PipelineGroup } from "..";
+export interface ProjectMobileUserConfiguration {
+    hubUserId: string;
+    hubProjectId: string;
+    name?: string;
+    username?: string;
+    email: string;
+    domains: string[];
+    pluginIds: string[];
+    projects: PipelineGroup[];
+}
