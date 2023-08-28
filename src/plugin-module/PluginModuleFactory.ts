a462a236570d9baf0e9d65a422d96845c042ce2c
Alec Jones <alecejones@noodoe.com>
2023-08-28 13:29:47 +0800

    fix: modify higher order functions

--- src/plugin-module/PluginModuleFactory.ts
+++ src/plugin-module/PluginModuleFactory.ts
@@ -11,51 +11,84 @@ import { SandboxLib } from "..";
 
 export type ActionReturnValue = Promise<DynamicServiceResponseOutput>;
 
-export type ActionReturnFunction = <T = any>(
+export type ActionReturnFunction<T = any> = (
   body: DynamicServiceBody,
   lib: SandboxLib,
   config?: T
-) => ActionReturnValue | ListenerReturnValue;
+) =>
+  | ActionReturnValue
+  | ListenerReturnValue
+  | ((
+      body: DynamicServiceBody,
+      lib: SandboxLib,
+      config: T
+    ) => ActionReturnValue | ListenerReturnValue);
 
 export type CredentialReturnValue =
   Promise<DynamicServiceResponseOutputCredentialAuthCallback>;
 
-export type CredentialReturnFunction = <T = any>(
+export type CredentialReturnFunction<T = any> = (
   body: DynamicServiceBody,
   lib: SandboxLib,
   config?: T
-) => CredentialReturnValue;
+) =>
+  | CredentialReturnValue
+  | ((
+      body: DynamicServiceBody,
+      lib: SandboxLib,
+      config: T
+    ) => CredentialReturnValue);
 
 export type CredentialGenerateAuthURIReturnValue =
   Promise<DynamicServiceResponseOutputCredentialGenerateAuthURI>;
 
-export declare type CredentialGenerateAuthURIFunction = <T = any>(
+export declare type CredentialGenerateAuthURIFunction<T = any> = (
   body: DynamicServiceBody,
-  lib: SandboxLib,
-  config?: T
-) => CredentialGenerateAuthURIReturnValue;
+  lib: SandboxLib
+) =>
+  | CredentialGenerateAuthURIReturnValue
+  | ((
+      body: DynamicServiceBody,
+      lib: SandboxLib,
+      config: T
+    ) => CredentialGenerateAuthURIReturnValue);
 
 export type CredentialTestReturnValue =
   Promise<DynamicServiceResponseOutputCredentialTest>;
 
-export declare type CredentialTestFunction = <T = any>(
+export declare type CredentialTestFunction<T = any> = (
   body: DynamicServiceBody,
-  lib: SandboxLib,
-  config?: T
-) => CredentialTestReturnValue;
+  lib: SandboxLib
+) =>
+  | CredentialTestReturnValue
+  | ((
+      body: DynamicServiceBody,
+      lib: SandboxLib,
+      config: T
+    ) => CredentialTestReturnValue);
 
 export type ListenerReturnValue = Promise<DynamicServiceResponseOutputListener>;
 
-export type ListenerReturnFunction = <T = any>(
+export type ListenerReturnFunction<T = any> = (
   body: DynamicServiceBody,
-  lib: SandboxLib,
-  config?: T
-) => ListenerReturnValue;
+  lib: SandboxLib
+) =>
+  | ListenerReturnValue
+  | ((
+      body: DynamicServiceBody,
+      lib: SandboxLib,
+      config: T
+    ) => ListenerReturnValue);
 
 export type WebhookReturnValue = Promise<DynamicServiceResponseOutputWebhook>;
 
-export type WebhookReturnFunction = <T = any>(
+export type WebhookReturnFunction<T = any> = (
   body: DynamicServiceBody,
-  lib: SandboxLib,
-  config?: T
-) => WebhookReturnValue;
+  lib: SandboxLib
+) =>
+  | WebhookReturnValue
+  | ((
+      body: DynamicServiceBody,
+      lib: SandboxLib,
+      config: T
+    ) => WebhookReturnValue);
