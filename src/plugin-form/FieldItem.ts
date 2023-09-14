b69eafeaff69a17fcf28cb18da9db321e5f558c9
Alec Jones <alecejones@noodoe.com>
2023-09-14 14:31:10 +0800

    fix: extended field item

--- src/plugin-form/FieldItem.ts
+++ src/plugin-form/FieldItem.ts
@@ -2,10 +2,65 @@ import { FieldPrerequisite } from ".";
 import { FieldDescriptor } from ".";
 import { FieldStatusMapItem } from "..";
 
+export type FieldItemType =
+  | "text"
+  | "number"
+  | "custom"
+  | "conditional"
+  | "select"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "header"
+  | "subheader"
+  | "category"
+  | "subcategory"
+  | "credential"
+  | "pipeline"
+  | "featuredGroup"
+  | "featuredThread"
+  | "repository"
+  | "repositoryFiles"
+  | "repositoryTag"
+  | "user"
+  | "component"
+  | "space"
+  | "checkbox"
+  | "colorPicker"
+  | "wysiwyg"
+  | "date"
+  | "roles"
+  | "link"
+  | "selectionHandler"
+  | "selectionHandlerId"
+  | "platforms"
+  | "password"
+  | "timezone"
+  | "chart"
+  | "csv"
+  | "csvReport"
+  | "dataStore"
+  | "json-transformation"
+  | "jsonTransformation"
+  | "jsonSchemaForm"
+  | "jsonView"
+  | "hideFields"
+  | "prompt"
+  | "webhookRoute"
+  | "descriptors"
+  | "empty"
+  | "image"
+  | "listener"
+  | "webhook"
+  | "collection";
+
 export interface FieldItem {
   value: string;
   label: string;
-  type?: string;
+  type?: FieldItemType;
   options?: FieldOption[] | ((field: FieldItem, props: any) => FieldOption[]);
   allowList?: Record<string, any>[];
   blockList?: Record<string, any>[];
@@ -30,6 +85,7 @@ export interface FieldItem {
   };
   hiddenFields?: FieldItem[];
   fieldStatus?: FieldStatusMapItem;
+  collection?: FieldItem[];
 }
 
 export interface FieldOption {
