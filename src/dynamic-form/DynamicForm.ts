250565ae3269c90107aeb5939f4a71229a7b57d3
wordparrot <86702665+wordparrot@users.noreply.github.com>
2024-09-21 16:50:39 -0500

    Update DynamicForm.ts (#9)

--- src/dynamic-form/DynamicForm.ts
+++ src/dynamic-form/DynamicForm.ts
@@ -1,6 +1,8 @@
 import { DynamicFormField, DynamicFormSpecification } from ".";
 import { PipelineNode, PromptInstance } from "..";
 
+
+// Dynamic Form type allows users to create their own forms to serve to their users. Not to be confused with prompts/prompt instances/prompt policies.
 export interface DynamicForm {
   id: string;
 
