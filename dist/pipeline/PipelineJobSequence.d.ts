41b2088472f77e893dd781e608bcbfbf0af964ef
Alec Jones <alecejones@gmail.com>
2024-10-03 12:10:06 -0500

    fix: add project info to sequence

--- dist/pipeline/PipelineJobSequence.d.ts
+++ dist/pipeline/PipelineJobSequence.d.ts
@@ -1,13 +1,17 @@
-import { PipelineJob } from ".";
+import { PipelineGroup, PipelineJob } from ".";
 import { Listener } from "../listener";
-import { PromptInstance } from "../prompt";
+import { PromptInstance, PromptRecipient } from "../prompt";
 import { Webhook } from "../webhook";
 export interface PipelineJobSequence {
     id: string;
     status: string;
     delay: number;
+    project: PipelineGroup;
+    projectId: string;
     promptInstance: PromptInstance;
     promptInstanceId: string;
+    promptRecipient: PromptRecipient;
+    promptRecipientId: string;
     listener: Listener;
     listenerId: string;
     webhook: Webhook;
